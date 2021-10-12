d9e830c084203bcef48009b308ffd985f4e92a8f
wnsgus <ggp032177@naver.com>
2021-10-12 17:34:08 +0900

    chage name

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "ros/ros.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "test_node");
+	ros::Time::init();
+	ros::Rate loop_rate(10);
+ 	while(ros::ok())
+   	{ 
+        ROS_INFO("Hello World!");
+		loop_rate.sleep();
+		ros::spinOnce();
+   	}
+}
